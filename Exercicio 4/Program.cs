7fd023977ed601850b6b79c37b7951f95d506ff1
Eduardo <eduzika2903@hotmail.com>
2020-11-25 13:27:52 -0300

    Exercicio 4

---
+++ Exercicio 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Faça um programa que leia 10 valores digitados pelo usuário e no final, escreva o maior e o
+            // menor valor lido.
+            Console.WriteLine("Escreva 10 números: ");
+            float numeros = float.Parse(Console.ReadLine());
+            
+             float maiornum = numeros;
+
+             float menornum = numeros;
+
+         for (var i = 0; i < 9; i++)
+         {
+                numeros = float.Parse(Console.ReadLine());
+            
+
+                if(numeros < menornum){
+                    menornum = numeros;
+                }
+                else if(numeros > maiornum){
+                    maiornum = numeros;
+                }
+            }
+
+            Console.WriteLine($"O maior numero é: {maiornum}");
+
+            Console.WriteLine($"O menor numero é: {menornum}");
+        }
+    }
+}
