b41fc2ae3055970db2d55246198a3a6e98bb0ee4
Eduardo <eduzika2903@hotmail.com>
2020-11-25 13:30:33 -0300

    Exercicio 1

---
+++ Idade-votacao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercício_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o ano que você nasceu: ");
+            int ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o ano atual: ");
+            int anoatual = int.Parse(Console.ReadLine());
+
+            int idade = anoatual - ano;
+
+            if(idade < 16){
+                Console.WriteLine($"Sua idade é de: {idade} anos, e você não possui idade mínima para votar este ano");
+            }else{
+                Console.WriteLine($"Parabéns! Você tem {idade} anos e poderá votar nesta eleição");
+            }
+        }
+    }
+}
---
+++ Idade-votacao/README.md
@@ -0,0 +1,2 @@
+# Idade-votacao
+Programa que lê o ano atual e o ano que você nasceu, e diz se poderá votar na eleição deste ano
